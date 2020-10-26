a25dcae779afe3b32a3e1c619c453ad09c3ec339
awais <ahmedawais5961@gmail.com>
2020-10-27 03:49:13 +0500

    reregistration fix

--- routes/registeredcourses.js
+++ routes/registeredcourses.js
@@ -53,7 +53,6 @@ router.post('/', auth, async (req, res) => {
     const { course_id } = req.body;
 
     const course = await Course.findById(course_id);
-
     if (!course) {
         res.status(404).send('Ooops no course with this detail is found');
         return;
@@ -62,13 +61,13 @@ router.post('/', auth, async (req, res) => {
         res.status(401).send('Course is not published and you can not register untill the course is published')
     }
 
-
-    const registercourse = await RegisterCourse.find({ course_id: mongoose.Types.ObjectId(course_id) });
-    //  const alreadyRegis = await Course.find({ course_id: mongoose.Types.ObjectId(course_id) });
-    if (registercourse) {
-        res.send("Course is already registered");
+    const alreadyRegistered = await RegisterCourse.findOne({ course_id: course_id, learner_id: req.user.id });
+    // res.send(alreadyRegistered)
+    if (alreadyRegistered) {
+        res.status(200).send('Course already registered')
     }
     else {
+
         try {
 
 
@@ -87,8 +86,76 @@ router.post('/', auth, async (req, res) => {
         }
     }
 
+
 });
 
+/**
+ * @route Put / api/courses/:id
+ * @description update course
+ * @access Private
+ */
+router.put('/:id', auth, async (req, res) => {
+
+    // let { error } = courseValidationSchema.validate(req.body);
+    // if (error) { return res.status(400).send(error.details[0].message) }
+    // // Pulling required Information from the Request 
+    // const { title, subTitle, description } = req.body;
+
+    // // Build contact object
+    // const updatedCourse = {};
+    // if (title) updatedCourse.title = title;
+    // if (subTitle) updatedCourse.subTitle = subTitle;
+    // if (description) updatedCourse.description = description;
+
+    // try {
+    //     let course = await Course.findById(req.params.id);
+
+    //     if (!course) return res.status(404).json({ msg: 'Course not found' });
+
+    //     // Make sure user owns contact
+    //     if (course.educator_id.toString() !== req.user.id)
+    //         return res.status(401).json({ msg: 'Not authorized' });
+
+    //     course = await Course.findByIdAndUpdate(
+    //         req.params.id,
+    //         { $set: updatedCourse },
+    //         { new: true }
+    //     );
+
+    //     res.json(course);
+    // } catch (err) {
+    //     console.error(err.message);
+    //     res.status(500).send('Server error');
+    // }
+    res.send('updated coruse');
+
+});
+
+/**
+ * @route DELETE / api/course
+ * @description remove course
+ * @access Private
+ */
+router.delete('/:id', auth, async (req, res) => {
+    try {
+
+        const course = await RegisterCourse.findById(req.params.id);
+        if (!course) return res.status(404).json({ msg: 'Course with this id does not exists' });
+
+        // Make sure user owns course
+        // if (course.learner_id.toString() !== req.user.id)
+        //     return res.status(401).json({ msg: 'Not authorized' });
+
+        await RegisterCourse.findByIdAndRemove(req.params.id);
+        res.status(200).json({ msg: 'Course removed' });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    res.send('unregister course')
+
+});
 
 // Schema Validation 
 const RegistercourseSchemaValidation = Joi.object({
